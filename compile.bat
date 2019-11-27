134e9d706c71fab205b43ff346c44fdd7b3453ed
Jordi Montornés Solé <jordi.montornes@gmail.com>
2019-11-27 18:47:46 +0000

    Added Lesson 1 for Shooter Tutorial

--- compile.bat
+++ compile.bat
@@ -6,11 +6,17 @@ del *.lst
 del *.sym
 echo Borrando rom
 del *.sms
+echo Reexportar fondos
+java -jar .\tools\ugt.jar -tiledfile  .\material\level1.tmx -destination .\assets -name level1
+echo Convertir ficheros de graficos a C y compilarlos
+assets2banks assets --compile --singleheader
+echo Compilar Librerias
+sdcc -c -mz80 --peep-file peep-rules.txt libs/GSLib.c
 echo Compilar Juego
 sdcc -c -mz80 --peep-file peep-rules.txt game.c
 if %errorlevel% neq 0 exit
 echo Linkar Juego
-sdcc -o shooter.ihx -mz80 --no-std-crt0 --data-loc 0xC000 crt0_sms.rel game.rel SMSlib.lib
+sdcc -o shooter.ihx -mz80 --no-std-crt0 --data-loc 0xC000 -Wl-b_BANK2=0x8000 crt0_sms.rel game.rel SMSlib.lib GSLib.rel bank2.rel
 if %errorlevel% neq 0 exit
 echo Convertir de binario a rom
 ihx2sms shooter.ihx shooter.sms
--- game.c
+++ game.c
@@ -1,7 +1,11 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include "assets2banks.h"
 #include "libs/SMSlib.h"
+#include "libs/GSLib.h"
 
+unsigned char deltax = 0;
+unsigned char deltay = 0;
 unsigned int frame_cnt = 0;
 
 void init_console() {
@@ -14,11 +18,24 @@ void init_console() {
 
 void main() {
 	init_console();
+    SMS_loadBGPalette(level1_palette_bin);    
+    SMS_loadTiles(&level1_tiles_bin, 0, level1_tiles_bin_size);
+    GSL_initializeMap(&level1_scrolltable_bin, &level1_metatiles_bin);
+	GSL_positionWindow(0,0);
+	GSL_refreshVDP();
     SMS_displayOn();
-
+    deltax = 1;
 	while (1) {
+        GSL_scroll(deltax,deltay);
+        if(frame_cnt == 100) {
+            deltax = 1;
+        }
+        if((GSL_getCurrentX() + 256) == GSL_getMapWidthInPixels()) {
+            deltax = 0;
+        }
         frame_cnt++;
-        SMS_waitForVBlank(); 
+        SMS_waitForVBlank();
+        GSL_VBlank(); 
     }
 }
 SMS_EMBED_SEGA_ROM_HEADER(9999, 0); // code 9999 hopefully free, here this means 'homebrew'
